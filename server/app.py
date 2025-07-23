ef4e76f8ef8e0aeaad1446b926e5a39bbb823950
Shelby Christensen <shelbchrist@yahoo.com>
2025-07-23 12:37:49 -0500

    Add route to get earthquake by ID with JSON response

--- server/app.py
+++ server/app.py
@@ -1,9 +1,8 @@
 # server/app.py
 #!/usr/bin/env python3
 
-from flask import Flask, make_response
+from flask import Flask, jsonify, make_response
 from flask_migrate import Migrate
-
 from models import db, Earthquake
 
 app = Flask(__name__)
@@ -11,17 +10,25 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.json.compact = False
 
-migrate = Migrate(app, db)
 db.init_app(app)
-
+migrate = Migrate(app, db)
 
 @app.route('/')
 def index():
     body = {'message': 'Flask SQLAlchemy Lab 1'}
     return make_response(body, 200)
 
-# Add views here
-
+@app.route('/earthquakes/<int:id>')
+def get_earthquake_by_id(id):
+    quake = Earthquake.query.filter_by(id=id).first()
+    if quake:
+        return jsonify({
+            "id": quake.id,
+            "location": quake.location,
+            "magnitude": quake.magnitude,
+            "year": quake.year
+        }), 200
+    return jsonify({"message": f"Earthquake {id} not found."}), 404
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
